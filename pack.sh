94e6aabb0573cd022abc01f1e0c4c453224b28b0
Sardor Muminov <sardor@insilicogen.com>
2016-08-02 11:23:12 +0900

    Pack script updated

--- pack.sh
+++ pack.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-alias split2="/usr/local/opt/coreutils/libexec/gnubin/split"
 cd src
 tar czvf documents.tar.gz *
 gpg -e -u "Sardor Muminov" -r "Sardor Muminov" documents.tar.gz
 rm documents.tar.gz
-split2 -a 4 -d -b 512k documents.tar.gz.gpg documents.tar.gz.gpg.
+/usr/local/opt/coreutils/libexec/gnubin/split -a 4 -d -b 512k documents.tar.gz.gpg documents.tar.gz.gpg.
 rm documents.tar.gz.gpg
-# mv documents.tar.tz.gpg.* ../
+mv documents.tar.gz.gpg.* ../documents/
