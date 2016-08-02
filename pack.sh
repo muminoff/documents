188aedef211c7b9c830b01b4629bb4648f0ec67b
Sardor Muminov <sardor@insilicogen.com>
2016-08-02 16:00:33 +0900

    Recipient

--- pack.sh
+++ pack.sh
@@ -4,7 +4,7 @@ set -e
 
 cd src
 tar czvf documents.tar.gz *
-gpg -a -e -u "Sardor Muminov" -r "Sardor Muminov" documents.tar.gz
+gpg -a -e -r 1371D515 documents.tar.gz
 rm documents.tar.gz
 /usr/local/opt/coreutils/libexec/gnubin/split -a 4 -d -b 512k documents.tar.gz.asc documents.tar.gz.asc.
 rm documents.tar.gz.asc
