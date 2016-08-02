0fdef407e211dfe8cf98a7bc07e3791505e129c3
Sardor Muminov <sardor@insilicogen.com>
2016-08-02 11:10:58 +0900

    Pack script

---
+++ pack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd src
+tar czvf documents.tar.gz .
+gpg -e -u "Sardor Muminov" -r "Sardor Muminov" documents.tar.gz
+rm documents.tar.gz
+split2 -a 4 -d -b 512k documents.tar.gz.gpg documents.tar.gz.gpg.
+rm documents.tar.gz.gpg
+mv documents.tar.tz.gpg.* ../
